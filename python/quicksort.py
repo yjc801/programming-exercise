0dc2f48a2e3254ab2b3fb43fac93ac38a83a274c
Junchao Yan <yjc801@gmail.com>
2014-01-09 23:52:40 -0500

    init

---
+++ python/quicksort.py
@@ -0,0 +1,20 @@
+def quicksort(A,p,r):
+	if (r-p)>0:
+		q = partition(A,p,r)
+		quicksort(A,p,q-1)
+		quicksort(A,q+1,r)
+
+def partition(A,p,r):
+	firsthigh = p
+	h = r
+	for i in xrange(p,r):
+		if A[i] < A[h]:
+			A[i],A[firsthigh] = A[firsthigh],A[i]
+			firsthigh = firsthigh + 1
+	A[r],A[firsthigh] = A[firsthigh],A[r]
+	return firsthigh
+
+if __name__ == '__main__':
+	A = [4,2,6,1,3,0,5]
+	quicksort(A,0,6)
+	print A
