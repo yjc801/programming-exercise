ddc24254a4bbddc7a86e0c36bb8617a02fd49b29
Junchao Yan <yjc801@gmail.com>
2014-01-12 21:10:16 -0500

    added bfs

--- basics/bfs.py
+++ basics/bfs.py
@@ -9,6 +9,7 @@ def creat_graph(edges): # unweighted, undirected
 
 def bfs(graph,start):
 	status = dict()
+	parents = {start:-1}
 	q = deque()
 	for key in graph.iterkeys():
 		status[key] = False
@@ -16,12 +17,24 @@ def bfs(graph,start):
 	status[start] = True
 	while q:
 		parent = q.popleft()
+		print "Processed vertex %d" % (parent)
 		for vertice in graph[parent]:
+			print "Processed edge %d -> %d" % (parent, vertice)
 			if not status[vertice]:
 				q.append(vertice)
 				status[vertice] = True
+				parents[vertice] = parent
+	return parents
+
+def find_path(start,end,parents):
+	if (start == end) or (end == -1):
+		print start
+	else:
+		find_path(start,parents[end],parents)
+		print end
 
 if __name__ == '__main__':
-	graph = creat_graph([[1,2],[2,3],[2,4],[1,3],[3,5],[4,5]])
+	graph = creat_graph([[1,2],[1,5],[1,6],[2,3],[2,5],[3,4],[5,4]])
 	print graph
-	bfs(graph,1)
+	parents = bfs(graph,1)
+	find_path(1,4,parents)
