ce2d2a62747e4a3306bc93ee7a92bb545a2c3c3f
Junchao Yan <yjc801@gmail.com>
2014-01-13 12:04:11 -0500

    added sqrt

---
+++ leetcode/sqrt.py
@@ -0,0 +1,17 @@
+def sqrt(x):
+	if x ==0:
+		return 0
+	elif x == 1:
+		return 1
+	l = 1
+	r = x
+	while r-l>1:
+		mid = (l+r)/2
+		if mid*mid <= x:
+			l = mid
+		else:
+			r = mid
+	return l
+
+if __name__ == '__main__':
+	print sqrt(121)
