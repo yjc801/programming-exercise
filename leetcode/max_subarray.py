f4cb6356e5ce61871bdaa633d23d262c0f797162
Junchao Yan <yjc801@gmail.com>
2014-01-15 10:31:27 -0500

    revised max_subarray

--- leetcode/max_subarray.py
+++ leetcode/max_subarray.py
@@ -1,14 +1,24 @@
+import sys
+
 def maxSubArray(A):
 	maxm = 0
 	for i in xrange(len(A)):
 		tot = 0
 		for j in A[i+1:]:
 			tot += j
-			if maxm < tot:
-				maxm = tot
+			maxm = max(maxm,tot)
 	return maxm
 
+def maxSubArray2(A):
+	tot = 0
+	maxm = -sys.maxint-1
+	for i in A:
+		tot+=i
+		maxm = max(maxm,tot)
+		if tot < 0:
+			tot = 0
+	return maxm
 
 if __name__ == '__main__':
 	A = [-2,1,-3,4,-1,2,1,-5,4]
-	print maxSubArray(A)
+	print maxSubArray2(A)
